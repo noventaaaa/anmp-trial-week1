28122dbc053b378b877ba13730d398c1c88bea01
DedeJP <s160420140@student.ubaya.ac.id>
2023-08-29 11:03:33 +0700

    Menambahkan Login Activity

--- app/src/main/java/com/dede/a160420042_160420140_homeworkw1/LoginActivity.kt
+++ app/src/main/java/com/dede/a160420042_160420140_homeworkw1/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.dede.a160420042_160420140_homeworkw1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 var data_user = arrayOf(
     User(
@@ -11,10 +13,31 @@ var data_user = arrayOf(
         2,"Dedek","87654321"
     )
 )
+
 class LoginActivity : AppCompatActivity() {
+    companion object{
+        var USER_NAME = "USER_NAME"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        btnLogin.setOnClickListener {
+            var name = txtName.text.toString()
+            var pass = txtPassword.text.toString()
+            for(data in data_user.indices){
+                if (name == data_user[data].name && pass == data_user[data].password){
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.putExtra(USER_NAME,name.toString())
+                    startActivity(intent)
+                    lblNotification.text = "..."
+                }
+                else{
+                    lblNotification.text = "Maaf kamu bukan kelompok kami :("
+                }
+            }
+        }
 
 
     }
--- app/src/main/java/com/dede/a160420042_160420140_homeworkw1/MainActivity.kt
+++ app/src/main/java/com/dede/a160420042_160420140_homeworkw1/MainActivity.kt
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        var USER_NAME = "USER_NAME"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var members = intent.getStringExtra(LoginActivity.USER_NAME).toString()
     }
 }
